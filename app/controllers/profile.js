2ae0720cbf6608eb123c86c2c9224764d6b9d802
Morena Barboni <morena.barboni@studenti.unicam.it>
2017-08-25 10:12:48 +0200

    Aggiunte query per la gestione degli utenti

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -14,6 +14,32 @@ module.exports.verify = function(req, res) {
   }
 };
 
+//Ottiene tutti gli utenti.
+module.exports.getAllUsers = function(req, res) {
+  if (!req.payload._id) {
+    res.status(401).json({
+      message: "UnauthorizedError: private profile"
+    });
+  } else {
+    User.find({}, function(err, data) {
+        res.send(data);
+    }).sort({ name: 1 });
+  }
+};
+
+//Cancella un utente selezionandolo tramite id
+module.exports.deleteUserById = function(req, res) {
+  User.remove(
+    {
+      _id: req.params._id
+    },
+    function(err, course) {
+      if (err) res.send(err);
+      res.json({ message: "User successfully deleted" });
+    }
+  );
+};
+
 //Per ottenere i dati delle iscrizioni degli studenti.
 module.exports.getFaculties = function(req, res) {
   if (!req.payload._id) {
@@ -21,8 +47,7 @@ module.exports.getFaculties = function(req, res) {
       message: "UnauthorizedError: private profile"
     });
   } else {
-    User.find({ usertype: "studente" }, {faculty:1, entryYear:1}, function(err, data) {
-      console.log(data);
+    User.find({ usertype: "studente" }, { faculty: 1, entryYear: 1 }, function(err, data ) {
       res.send(data);
     });
   }

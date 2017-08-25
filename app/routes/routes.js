32518525ae42ad9079bcb9a8d22c2bf4c262d868
Morena Barboni <morena.barboni@studenti.unicam.it>
2017-08-25 10:13:10 +0200

    Aggiunte routes per la gestione degli utenti

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -12,17 +12,22 @@ router.get("/profile", auth, ctrlProfile.verify);
 router.get("/facultiesInfo", auth, ctrlProfile.getFaculties); //Per ottenere i dati delle iscrizioni degli studenti.
 
 // Autenticazione e Registrazione Utente
-router.post("/register", ctrlAuth.registerUser);
-router.post("/login", ctrlAuth.login);
+router.post("/register", ctrlAuth.registerUser); //registrazione utente
+router.post("/login", ctrlAuth.login); //login utente
 
 //Piano di Studi Studente
 router.get("/studentPlan", auth, ctrlPlan.getStudentPlan); //Ottiene il piano di studi per uno studente
 
 //Corsi Docente
-router.get("/professorPlan/:email", auth, ctrlPlan.getProfessorCoursesInfo) //Ottiene tutte le informazioni sui corsi tenuti da un docente
+router.get("/professorPlan/:email", auth, ctrlPlan.getProfessorCoursesInfo); //Ottiene tutte le informazioni sui corsi tenuti da un docente
 
 //Gestione Piano di Studi Admin
 router.get("/plan", auth, ctrlPlan.getAllPlan); //Restituisce tutti i piani
 router.post("/plan", ctrlPlan.addCourse); //Aggiunge un corso
 router.delete("/plan/:courseId", ctrlPlan.deleteCourseById); //Cancella un corso
+
+//Gestione utenti Admin
+router.get("/users", auth, ctrlProfile.getAllUsers); //Ottiene tutti gli utenti
+router.delete("/profile/:_id", ctrlProfile.deleteUserById); //Elimina un utente tramite Id
+
 module.exports = router;

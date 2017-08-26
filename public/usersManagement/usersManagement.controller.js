76b4f3d43d016315572aafe36bb3f281c1b3dc1f
Morena Barboni <morena.barboni@studenti.unicam.it>
2017-08-26 14:51:54 +0200

    Aggiunti filtri di ricerca per la gestione degli utenti

--- public/usersManagement/usersManagement.controller.js
+++ public/usersManagement/usersManagement.controller.js
@@ -1,12 +1,50 @@
 (function() {
-  angular.module("planApp").controller("usersManagementCtrl", usersManagementCtrl);
-  usersManagementCtrl.$inject = ["$location","userService","$http","$window","$scope","$window"];
-   function usersManagementCtrl($location, userService, $http, $window, $scope, $location ) {
+  angular
+    .module("planApp")
+    .controller("usersManagementCtrl", usersManagementCtrl);
+  usersManagementCtrl.$inject = [
+    "$location",
+    "userService",
+    "$http",
+    "$window",
+    "$scope",
+    "$window"
+  ];
+  function usersManagementCtrl(
+    $location,
+    userService,
+    $http,
+    $window,
+    $scope,
+    $location
+  ) {
     var vm = this;
 
     vm.user = {}; //Utente corrente
 
     vm.users = []; //Elenco di tutti gli utenti
+
+
+    //Filtri utente
+    vm.filter = {
+      filterName: "",
+      filterMat: "",
+      filterType: "",
+      filterEmail: ""
+    };
+
+    //Determina quali righe della tabella filtrare
+    vm.checkCorrespondence = function(name, mat, userType, email) {
+      if (
+        name.indexOf(vm.filter.filterName) !== -1 &&
+        mat.indexOf(vm.filter.filterMat) !== -1 &&
+        userType.indexOf(vm.filter.filterType) !== -1 &&
+        email.indexOf(vm.filter.filterEmail) !== -1
+      ) {
+        return true;
+      }
+      return false;
+    };
 
     initController();
 
@@ -31,7 +69,7 @@
         if (confirm("Vuoi davvero eliminare il tuo account?") == true) {
           userService.deleteUser(id);
           alert("Il tuo account è stato eliminato con successo!");
-          $window.location.href = 'logout';
+          $window.location.href = "logout";
         }
       } else {
         if (confirm("Vuoi davvero eliminare l'utente?") == true) {

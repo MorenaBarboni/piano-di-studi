f772e45d207080ec59dabd56ab3df5f72724357c
Morena Barboni <morena.barboni@studenti.unicam.it>
2017-09-08 17:45:14 +0200

    Aggiunto script per il pulsante a fondo pagina

---
+++ public/scripts/backToTopScript.js
@@ -0,0 +1,20 @@
+if (window.location.href.indexOf("profile") !== -1) {
+  window.onscroll = function() {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      document.getElementById("goToTop").style.display = "block";
+    } else {
+      document.getElementById("goToTop").style.display = "none";
+    }
+  }
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+}

28d8128c8f8cc2633c7bea9b410a4fc677739b5c
costanzacesanelli <costanza.cesanelli@gmail.com>
2017-09-16 09:52:47 +0200

    aggiunta mappa sulla home

--- public/home/home.controller.js
+++ public/home/home.controller.js
@@ -1,8 +1,8 @@
 (function() {
   angular.module("planApp").controller("homeCtrl", homeCtrl);
 
-  homeCtrl.$inject = ["$location", "userService"];
-  function homeCtrl($location, userService) {
+  homeCtrl.$inject = ["$location", "userService", "$scope"];
+  function homeCtrl($location, userService, $scope) {
     var vm = this;
 
     vm.user = [];
@@ -10,11 +10,72 @@
     //email degli admin per il footer
     vm.contacts = {};
 
+    var cities = [
+      {
+        city:  "Polo di Informatica",
+        desc: "",
+        lat: 43.1395461,
+        long: 13.0687677
+      },
+      {
+        city: "Polo di Fisica",
+        desc: "",
+        lat: 43.139528,
+        long: 13.0677438
+      },
+      {
+        city:"Polo di Chimica",
+        desc: "",
+        lat: 43.13869427,
+        long: 13.06986541
+      }
+    ];
+
+    var mapOptions = {
+      zoom: 16,
+      center: new google.maps.LatLng(43.139528, 13.0677438),
+      mapTypeId: google.maps.MapTypeId.SATELLITE
+    };
+
+    $scope.map = new google.maps.Map(
+      document.getElementById("map"),
+      mapOptions
+    );
+
+    $scope.markers = [];
+
+    var infoWindow = new google.maps.InfoWindow();
+
+    var createMarker = function(info) {
+      var marker = new google.maps.Marker({
+        map: $scope.map,
+        position: new google.maps.LatLng(info.lat, info.long),
+        title: info.city
+      });
+      marker.content = '<div class="infoWindowContent">' + info.desc + "</div>";
+
+      google.maps.event.addListener(marker, "click", function() {
+        infoWindow.setContent("<h2>" + marker.title + "</h2>" + marker.content);
+        infoWindow.open($scope.map, marker);
+      });
+
+      $scope.markers.push(marker);
+    };
+
+    for (i = 0; i < cities.length; i++) {
+      createMarker(cities[i]);
+    }
+
+    $scope.openInfoWindow = function(e, selectedMarker) {
+      e.preventDefault();
+      google.maps.event.trigger(selectedMarker, "click");
+    };
+
     initController(); //Inizializza il controller
 
     function initController() {
       userService.getAdminEmails().then(function(data) {
-        vm.contacts = data;      
+        vm.contacts = data;
       });
       userService
         .getProfile()

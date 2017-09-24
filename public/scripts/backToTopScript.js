0ba023006a269b956c926ff43c4698f3dbd19b4b
Morena Barboni <morena.barboni@studenti.unicam.it>
2017-09-24 14:56:12 +0200

    Aggiunte pagine allo script per il back to top

--- public/scripts/backToTopScript.js
+++ public/scripts/backToTopScript.js
@@ -1,6 +1,14 @@
 if (
   window.location.href.indexOf("profile") !== -1 ||
-  window.location.href.indexOf("career") !== -1
+  window.location.href.indexOf("career") !== -1  ||
+  window.location.href.indexOf("examSession") !== -1 ||
+  window.location.href.indexOf("examBooking") !== -1 ||
+  window.location.href.indexOf("examSession/validation") !== -1||
+  window.location.href.indexOf("plan") !== -1||
+  window.location.href.indexOf("thesis") !== -1||
+  window.location.href.indexOf("usersManagement") !== -1||
+  window.location.href.indexOf("statistics") !== -1
+  
 ) {
   window.onscroll = function() {
     scrollFunction();
